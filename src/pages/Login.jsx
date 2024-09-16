e8fb93f386a67824abffc14ddb3cd4a6acc2f98f
chunjull <chunjuchein@gmail.com>
2024-09-16 09:30:59 +0800

    feat: login account by email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,21 +4,50 @@ import { registerUser } from "../services/api";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isRegister, setIsRegister] = useState(true);
 
   const handleRegister = async () => {
     await registerUser(email, password);
   };
 
+  const handleLogin = async () => {
+    console.log("Login with", email, password);
+  };
+
   return (
-    <div className="flex flex-col">
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email" placeholder="email" className="border p-3" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" placeholder="password" className="border p-3" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button className="border p-3 mt-4" onClick={handleRegister}>
-        Register
-      </button>
+    <div className="p-4 flex flex-col">
+      <div className="flex justify-between mb-4">
+        <button className={`border p-3 ${isRegister ? "bg-gray-200" : ""}`} onClick={() => setIsRegister(true)}>
+          Register
+        </button>
+        <button className={`border p-3 ${!isRegister ? "bg-gray-200" : ""}`} onClick={() => setIsRegister(false)}>
+          Login
+        </button>
+      </div>
+
+      {isRegister ? (
+        <div className="flex flex-col">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" placeholder="email" className="border p-3" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" placeholder="password" className="border p-3" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button className="border p-3 mt-4" onClick={handleRegister}>
+            Register
+          </button>
+        </div>
+      ) : (
+        <div className="flex flex-col">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" placeholder="email" className="border p-3" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" placeholder="password" className="border p-3" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <button className="border p-3 mt-4" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
+      )}
     </div>
   );
 }
+
 export default Login;

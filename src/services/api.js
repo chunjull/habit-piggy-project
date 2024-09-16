008e6a80e75d98b5c649a2fd52fef593bec11782
chunjull <chunjuchein@gmail.com>
2024-09-16 09:52:55 +0800

    feat: initialize user profile

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import db from "../utils/firebaseConfig";
 import { getAuth, createUserWithEmailAndPassword, signOut } from "firebase/auth";
-import { collection, addDoc } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 
 const auth = getAuth();
 async function registerUser(email, password) {
@@ -9,11 +9,21 @@ async function registerUser(email, password) {
     const user = userCredential.user;
     console.log("User created: ", user);
 
-    const docRef = await addDoc(collection(db, "users"), {
+    const userDocRef = doc(db, "users", user.uid);
+    await setDoc(userDocRef, {
       uid: user.uid,
       email: user.email,
+      name: "",
+      introduction: "",
+      avatar: "",
+      createdTime: Date.now(),
+      levelPoints: 0,
+      isAcceptReminder: false,
+      achievements: [],
+      badges: [],
+      habit: [],
     });
-    console.log("Document written with ID: ", docRef.id);
+    console.log("User data created with ID: ", user.uid);
   } catch (error) {
     console.error("Error creating user: ", error.code, error.message);
   }

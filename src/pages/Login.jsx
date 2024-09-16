6a0a67755941c5d27cb528d32a35945629a454c1
chunjull <chunjuchein@gmail.com>
2024-09-16 09:21:54 +0800

    feat: register account by firebase authentication

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,22 @@
-import { addUser } from "../services/api";
+import { useState } from "react";
+import { registerUser } from "../services/api";
 
 function Login() {
-  const handleAddUser = async () => {
-    await addUser();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async () => {
+    await registerUser(email, password);
   };
 
   return (
     <div className="flex flex-col">
       <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email" placeholder="email" className="border p-3" />
+      <input type="text" name="email" id="email" placeholder="email" className="border p-3" value={email} onChange={(e) => setEmail(e.target.value)} />
       <label htmlFor="password">Password</label>
-      <input type="password" name="password" id="password" placeholder="password" className="border p-3" />
-      <button className="border p-3 mt-4" onClick={handleAddUser}>
-        Login
+      <input type="password" name="password" id="password" placeholder="password" className="border p-3" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button className="border p-3 mt-4" onClick={handleRegister}>
+        Register
       </button>
     </div>
   );
--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,22 @@
 import db from "../utils/firebaseConfig";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { collection, addDoc } from "firebase/firestore";
 
-async function addUser() {
+const auth = getAuth();
+async function registerUser(email, password) {
   try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+    console.log("User created: ", user);
+
     const docRef = await addDoc(collection(db, "users"), {
-      account: "",
-      email: "",
+      uid: user.uid,
+      email: user.email,
     });
     console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
+  } catch (error) {
+    console.error("Error creating user: ", error.code, error.message);
   }
 }
 
-export { addUser };
+export { registerUser };

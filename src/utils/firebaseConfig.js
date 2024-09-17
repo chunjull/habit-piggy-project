5db1f67c647b9a10f429b491c5a7c10d5f8a1059
chunjull <chunjuchein@gmail.com>
2024-09-17 11:18:14 +0800

    chore: adjust firebase config file

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoA9vGq5gsjRVqngQcfX9wDHv03MFX8Mo",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "habit-piggy-project.firebaseapp.com",
   projectId: "habit-piggy-project",
   storageBucket: "habit-piggy-project.appspot.com",

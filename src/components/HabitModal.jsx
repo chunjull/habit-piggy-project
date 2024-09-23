6cfb97a845191c3c9d518e21537f10a3214a8ff6
chunjull <chunjuchein@gmail.com>
2024-09-23 11:02:08 +0800

    fix: fix add habit issues

--- src/components/HabitModal.jsx
+++ src/components/HabitModal.jsx
@@ -2,12 +2,24 @@ import MonthCalendar from "./MonthCalendar";
 import PropTypes from "prop-types";
 import CategorySelect from "./CategorySelect";
 
-const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, showMonthCalendar, calendarTarget, selectedDate, handleSelectDate, calendarRef, handleHabitModal, habitCategories }) => (
+const HabitModal = ({
+  habitData,
+  handleHabitChange,
+  handleAddHabit,
+  handleFocus,
+  showMonthCalendar,
+  calendarTarget,
+  selectedDate,
+  handleSelectDate,
+  calendarRef,
+  handleHabitModal,
+  habitCategories,
+}) => (
   <div className="space-y-4">
     <div className="flex justify-between gap-4">
       <div className="flex gap-4 w-full">
-        <CategorySelect options={habitCategories} value={habitData.category} onChange={(value) => handleChange({ target: { name: "category", value } })} />
-        <input type="text" name="title" placeholder="輸入習慣名稱" className="py-2 px-4 w-full" value={habitData.title} onChange={handleChange} />
+        <CategorySelect options={habitCategories} value={habitData.category} onChange={(value) => handleHabitChange({ target: { name: "category", value } })} />
+        <input type="text" name="title" placeholder="輸入習慣名稱" className="py-2 px-4 w-full" value={habitData.title} onChange={handleHabitChange} />
       </div>
       <button onClick={handleHabitModal}>close</button>
     </div>
@@ -21,7 +33,7 @@ const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, show
           value="daily"
           className="appearance-none h-4 w-4 border border-gray-300 rounded-full checked:bg-slate-500 checked:border-transparent focus:outline-none"
           checked={habitData.frequency === "daily"}
-          onChange={handleChange}
+          onChange={handleHabitChange}
         />
         <label htmlFor="daily">每日</label>
       </div>
@@ -33,7 +45,7 @@ const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, show
           value="weekly"
           className="appearance-none h-4 w-4 border border-gray-300 rounded-full checked:bg-slate-500 checked:border-transparent focus:outline-none"
           checked={habitData.frequency === "weekly"}
-          onChange={handleChange}
+          onChange={handleHabitChange}
         />
         <label htmlFor="weekly">每週</label>
       </div>
@@ -45,7 +57,7 @@ const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, show
           value="specificDays"
           className="appearance-none h-4 w-4 border border-gray-300 rounded-full checked:bg-slate-500 checked:border-transparent focus:outline-none"
           checked={habitData.frequency === "specificDays"}
-          onChange={handleChange}
+          onChange={handleHabitChange}
         />
         <label htmlFor="specificDay">特定日期</label>
       </div>
@@ -54,7 +66,7 @@ const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, show
       <label htmlFor="amount">習慣罰款</label>
       <div className="flex gap-2">
         <p>NT$</p>
-        <input type="number" name="amount" id="amount" className="px-4" value={habitData.amount} onChange={handleChange} />
+        <input type="number" name="amount" id="amount" className="px-4" value={habitData.amount} onChange={handleHabitChange} />
       </div>
     </div>
     <div className="flex justify-between gap-4 w-full">
@@ -70,7 +82,7 @@ const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, show
           placeholder="開始日期"
           value={habitData.startDate}
           onFocus={() => handleFocus("startDate")}
-          onChange={handleChange}
+          onChange={handleHabitChange}
         />
         {showMonthCalendar && calendarTarget === "startDate" && (
           <div ref={calendarRef} className="absolute z-10 bg-white shadow-lg w-[300px]">
@@ -80,7 +92,7 @@ const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, show
       </div>
       <p>~</p>
       <div className="relative">
-        <input type="text" name="endDate" id="endDate" className="text-center" placeholder="結束日期" value={habitData.endDate} onFocus={() => handleFocus("endDate")} onChange={handleChange} />
+        <input type="text" name="endDate" id="endDate" className="text-center" placeholder="結束日期" value={habitData.endDate} onFocus={() => handleFocus("endDate")} onChange={handleHabitChange} />
         {showMonthCalendar && calendarTarget === "endDate" && (
           <div ref={calendarRef} className="absolute z-10 bg-white shadow-lg w-[300px]">
             <MonthCalendar date={selectedDate} onSelect={handleSelectDate} />
@@ -96,7 +108,7 @@ const HabitModal = ({ habitData, handleChange, handleAddHabit, handleFocus, show
 
 HabitModal.propTypes = {
   habitData: PropTypes.object.isRequired,
-  handleChange: PropTypes.func.isRequired,
+  handleHabitChange: PropTypes.func.isRequired,
   handleAddHabit: PropTypes.func.isRequired,
   handleFocus: PropTypes.func.isRequired,
   showMonthCalendar: PropTypes.bool.isRequired,

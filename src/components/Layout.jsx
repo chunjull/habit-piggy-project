a0bddb8759c7d4eb13abdb270ceceac4ad1c26e7
chunjull <chunjuchein@gmail.com>
2024-09-25 11:36:28 +0800

    style: adjust styles of layout according to the design draft

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,40 +7,37 @@ function Layout({ children, isModalOpen, modalContent }) {
   const location = useLocation();
 
   return (
-    <div className="flex flex-col md:flex-row min-h-screen relative">
-      <nav className="fixed inset-x-0 bottom-0 md:w-[244px] pt-4 pb-3 px-4 border bg-white z-10 md:fixed md:top-0 md:left-0 md:h-full md:overflow-auto">
-        <ul className="grid grid-cols-6 items-center md:grid-cols-1 md:gap-6">
-          {/* <li className="p-2">
-            <Link to="/">Habit Piggy</Link>
-          </li> */}
-          <li className={`p-2 rounded-lg hover:md:bg-yellow-300 ${location.pathname === "/home" ? "md:bg-yellow-400" : ""}`}>
-            <Link to="/home" className="flex flex-col items-center md:flex-row">
-              <layoutIcons.TbHome2 className={`w-6 h-6 md:mr-4 ${location.pathname === "/home" ? "text-yellow-400 md:text-white" : ""}`} />
-              <p>Home</p>
+    <div className="flex flex-col md:flex-row min-h-screen relative font-sans bg-light">
+      <nav className="fixed inset-x-0 bottom-0 md:w-[244px] pt-2 pb-3 px-4 md:p-4 bg-black-50 z-10 md:fixed md:top-0 md:left-0 md:h-full md:overflow-auto">
+        <ul className="grid grid-cols-5 items-center md:grid-cols-1 md:gap-4">
+          <li className={`md:p-2 md:rounded-lg hover:md:bg-primary-light ${location.pathname === "/home" ? "md:bg-primary" : ""}`}>
+            <Link to="/home" className="flex flex-col gap-1 md:gap-0 items-center md:flex-row">
+              <layoutIcons.TbHome2 className={`w-6 h-6 md:mr-4 ${location.pathname === "/home" ? "text-primary md:text-black-50" : ""}`} />
+              <p className="text-black font-normal text-xs leading-4 md:text-base md:leading-6">主頁</p>
             </Link>
           </li>
-          <li className={`p-2 rounded-lg hover:md:bg-yellow-300 ${location.pathname === "/savings" ? "md:bg-yellow-400" : ""}`}>
+          <li className={`md:p-2 md:rounded-lg hover:md:bg-primary-light ${location.pathname === "/savings" ? "md:bg-primary" : ""}`}>
             <Link to="/savings" className="flex flex-col items-center md:flex-row">
-              <layoutIcons.TbCoin className={`w-6 h-6 md:mr-4 ${location.pathname === "/savings" ? "text-yellow-400 md:text-white" : ""}`} />
-              <p>Savings</p>
+              <layoutIcons.TbCoin className={`w-6 h-6 md:mr-4 ${location.pathname === "/savings" ? "text-primary md:text-black-50" : ""}`} />
+              <p className="text-black font-normal text-xs leading-4 md:text-base md:leading-6">存款</p>
             </Link>
           </li>
-          <li className={`p-2 rounded-lg hover:md:bg-yellow-300 ${location.pathname === "/rank" ? "md:bg-yellow-400" : ""}`}>
+          <li className={`md:p-2 md:rounded-lg hover:md:bg-primary-light ${location.pathname === "/rank" ? "md:bg-primary" : ""}`}>
             <Link to="/rank" className="flex flex-col items-center md:flex-row">
-              <layoutIcons.TbMedal2 className={`w-6 h-6 md:mr-4 ${location.pathname === "/rank" ? "text-yellow-400 md:text-white" : ""}`} />
-              <p>Rank</p>
+              <layoutIcons.TbMedal2 className={`w-6 h-6 md:mr-4 ${location.pathname === "/rank" ? "text-primary md:text-black-50" : ""}`} />
+              <p className="text-black font-normal text-xs leading-4 md:text-base md:leading-6">排行</p>
             </Link>
           </li>
-          <li className={`p-2 rounded-lg hover:md:bg-yellow-300 ${location.pathname === "/posts" ? "md:bg-yellow-400" : ""}`}>
+          <li className={`md:p-2 md:rounded-lg hover:md:bg-primary-light ${location.pathname === "/posts" ? "md:bg-primary" : ""}`}>
             <Link to="/posts" className="flex flex-col items-center md:flex-row">
-              <layoutIcons.TbMessage className={`w-6 h-6 md:mr-4 ${location.pathname === "/posts" ? "text-yellow-400 md:text-white" : ""}`} />
-              <p>Posts</p>
+              <layoutIcons.TbMessage className={`w-6 h-6 md:mr-4 ${location.pathname === "/posts" ? "text-primary md:text-black-50" : ""}`} />
+              <p className="text-black font-normal text-xs leading-4 md:text-base md:leading-6">貼文</p>
             </Link>
           </li>
-          <li className={`p-2 rounded-lg hover:md:bg-yellow-300 ${location.pathname === "/member" ? "md:bg-yellow-400" : ""}`}>
+          <li className={`md:p-2 md:rounded-lg hover:md:bg-primary-light ${location.pathname === "/member" ? "md:bg-primary" : ""}`}>
             <Link to="/member" className="flex flex-col items-center md:flex-row">
-              <layoutIcons.TbMoodSmile className={`w-6 h-6 md:mr-4 ${location.pathname === "/member" ? "text-yellow-400 md:text-white" : ""}`} />
-              <p>Member</p>
+              <layoutIcons.TbMoodSmile className={`w-6 h-6 md:mr-4 ${location.pathname === "/member" ? "text-primary md:text-black-50" : ""}`} />
+              <p className="text-black font-normal text-xs leading-4 md:text-base md:leading-6">會員</p>
             </Link>
           </li>
         </ul>

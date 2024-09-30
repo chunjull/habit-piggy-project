50189f41438d0d515ddebb5ee25e43efa5bf58cd
chunjull <chunjuchein@gmail.com>
2024-09-30 15:46:25 +0800

    style: adjust login&register page layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -40,31 +40,74 @@ function Login() {
   return (
     <div className="p-4 space-y-4">
       <ul className="grid grid-cols-2 w-full">
-        <li className={`border p-2 text-center ${isRegister ? "bg-gray-200" : ""}`} onClick={() => setIsRegister(true)}>
+        <li className={`border border-black-500 rounded-s-full py-1 font-normal text-sm leading-5 text-center ${isRegister ? "bg-primary" : "bg-black-50"}`} onClick={() => setIsRegister(true)}>
           Login
         </li>
-        <li className={`border p-2 text-center ${!isRegister ? "bg-gray-200" : ""}`} onClick={() => setIsRegister(false)}>
+        <li
+          className={`border-e border-y border-black-500 rounded-e-full py-1 font-normal text-sm leading-5 text-center ${!isRegister ? "bg-primary" : "bg-black-50"}`}
+          onClick={() => setIsRegister(false)}
+        >
           Register
         </li>
       </ul>
 
       {isRegister ? (
         <div className="flex flex-col">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" placeholder="email" className="border p-3" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" placeholder="password" className="border p-3" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button className="border p-3 mt-4" onClick={handleLogin}>
+          <label htmlFor="email" className="font-bold text-base leading-6 mb-2">
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            className="py-2 px-4 w-full rounded-xl border border-black-300 caret-primary-dark focus:border-primary-dark focus:outline focus:outline-primary-dark font-normal text-base leading-6 mb-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password" className="font-bold text-base leading-6 mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            className="py-2 px-4 w-full rounded-xl border border-black-300 caret-primary-dark focus:border-primary-dark focus:outline focus:outline-primary-dark font-normal text-base leading-6 mb-8"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="w-full rounded-lg bg-primary font-bold text-base leading-6 py-2 hover:bg-primary-dark" onClick={handleLogin}>
             Login
           </button>
         </div>
       ) : (
         <div className="flex flex-col">
-          <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" placeholder="email" className="border p-3" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="password" placeholder="password" className="border p-3" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button className="border p-3 mt-4" onClick={handleRegister}>
+          <label htmlFor="email" className="font-bold text-base leading-6 mb-2">
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            placeholder="Email"
+            className="py-2 px-4 w-full rounded-xl border border-black-300 caret-primary-dark focus:border-primary-dark focus:outline focus:outline-primary-dark font-normal text-base leading-6 mb-4"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password" className="font-bold text-base leading-6 mb-2">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            className="py-2 px-4 w-full rounded-xl border border-black-300 caret-primary-dark focus:border-primary-dark focus:outline focus:outline-primary-dark font-normal text-base leading-6 mb-8"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="w-full rounded-lg bg-primary font-bold text-base leading-6 py-2 hover:bg-primary-dark" onClick={handleRegister}>
             Register
           </button>
         </div>

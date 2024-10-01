4f19dd58f97093e91773172516680da751db6ceb
chunjull <chunjuchein@gmail.com>
2024-10-01 23:33:46 +0800

    refactor: adjust achievement list

--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -26,6 +26,7 @@ import EditModal from "../components/EditModal";
 import { Navigate } from "react-router-dom";
 import { habitIcons, settingIcons } from "../assets/icons";
 import HabitList from "../components/HabitList";
+import AchievementList from "../components/AchievementList";
 import CustomSelect from "../components/CustomSelect";
 import AchievementModal from "../components/AchievementModal";
 import BadgeModal from "../components/BadgeModal";
@@ -486,36 +487,7 @@ function Member() {
               <p className="font-normal text-base leading-6 text-black-500">{profileData.introduction}</p>
               <div className="w-full bg-light text-center rounded-2xl">{profileData.levelPoints}%</div>
             </div>
-            <div className="pt-9 pb-4 px-4 bg-black-50 space-y-4 rounded-2xl relative">
-              <ul className="grid grid-cols-2 gap-4 md:grid-cols-3">
-                {sortedAchievements.slice(0, 6).map((achievement) => (
-                  <li
-                    key={achievement.id}
-                    className={`py-1 w-full flex justify-center items-center rounded-lg border-2 cursor-default ${
-                      userAchievements.includes(achievement.id)
-                        ? "opacity-100 border-primary-dark bg-light text-primary-dark bordered-achievement"
-                        : "opacity-50 border-black-500 bg-black-100 text-black-500"
-                    }`}
-                  >
-                    <div className="font-normal text-base leading-6">{achievement.name}</div>
-                  </li>
-                ))}
-              </ul>
-              <button className="text-center w-full bg-primary rounded-xl font-medium text-sm leading-5 py-1 hover:bg-primary-dark" onClick={handleAchievementModal}>
-                更多成就
-              </button>
-              <div className="bg-primary py-1 px-4 w-fit absolute -top-12 left-1/2 transform -translate-x-1/2">
-                <p className="font-lobster font-normal text-2xl leading-8 text-alert">Achievement</p>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-16px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-t-[12px] before:border-l-primary before:border-r-transparent before:border-t-primary-dark before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[114px] before:w-0 before:h-0 before:border-r-[20px] before:border-l-0 before:border-t-[12px] before:border-r-primary before:border-l-transparent before:border-t-primary-dark before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-46px] before:w-8 before:h-10 before:bg-primary"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[134px] before:w-8 before:h-10 before:bg-primary"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[166px] before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-b-[20px] before:border-l-transparent before:border-r-transparent before:border-b-primary before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-1 before:left-[166px] before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-t-[20px] before:border-l-transparent before:border-r-transparent before:border-t-primary before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-66px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-b-[20px] before:border-l-transparent before:border-r-transparent before:border-b-primary before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-1 before:left-[-66px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-t-[20px] before:border-l-transparent before:border-r-transparent before:border-t-primary before:z-20"></div>
-              </div>
-            </div>
+            <AchievementList sortedAchievements={sortedAchievements} userAchievements={userAchievements} handleAchievementModal={handleAchievementModal} />
             <div className="pt-9 pb-4 px-4 bg-black-50 space-y-4 rounded-2xl relative">
               <ul className="grid grid-cols-3 gap-4 md:grid-cols-6">
                 {sortedBadges.slice(0, 6).map((badge, index) => (

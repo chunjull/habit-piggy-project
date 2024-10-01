e36a6d1c7aaae44dca69fa69e12c02b2d5ea9046
chunjull <chunjuchein@gmail.com>
2024-10-01 23:36:26 +0800

    refactor: adjust badge list

---
+++ src/components/BadgeList.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+const BadgeList = ({ sortedBadges, userBadges, handleBadgeModal }) => {
+  return (
+    <div className="pt-9 pb-4 px-4 bg-black-50 space-y-4 rounded-2xl relative">
+      <ul className="grid grid-cols-3 gap-4 md:grid-cols-6">
+        {sortedBadges.slice(0, 6).map((badge, index) => (
+          <li key={badge.id} className="relative w-full h-fit">
+            <img src={badge.image} alt={`Badge ${index}`} className={`w-full h-full object-cover ${userBadges.includes(badge.id) ? "opacity-100" : "opacity-30"}`} />
+          </li>
+        ))}
+      </ul>
+      <button className="text-center w-full bg-primary rounded-xl font-medium text-sm leading-5 py-1 hover:bg-primary-dark" onClick={handleBadgeModal}>
+        更多獎勵徽章
+      </button>
+      <div className="bg-primary py-1 px-4 w-[150px] absolute -top-12 left-1/2 transform -translate-x-1/2">
+        <p className="font-lobster font-normal text-2xl leading-8 text-alert text-center">Badge</p>
+        <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-16px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-t-[13px] before:border-l-primary before:border-r-transparent before:border-t-primary-dark before:z-20"></div>
+        <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[114px] before:w-0 before:h-0 before:border-r-[20px] before:border-l-0 before:border-t-[13px] before:border-r-primary before:border-l-transparent before:border-t-primary-dark before:z-20"></div>
+        <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-46px] before:w-8 before:h-10 before:bg-primary"></div>
+        <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[134px] before:w-8 before:h-10 before:bg-primary"></div>
+        <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[166px] before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-b-[20px] before:border-l-transparent before:border-r-transparent before:border-b-primary before:z-20"></div>
+        <div className="absolute before:content-[''] before:absolute before:bottom-1 before:left-[166px] before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-t-[20px] before:border-l-transparent before:border-r-transparent before:border-t-primary before:z-20"></div>
+        <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-66px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-b-[20px] before:border-l-transparent before:border-r-transparent before:border-b-primary before:z-20"></div>
+        <div className="absolute before:content-[''] before:absolute before:bottom-1 before:left-[-66px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-t-[20px] before:border-l-transparent before:border-r-transparent before:border-t-primary before:z-20"></div>
+      </div>
+    </div>
+  );
+};
+
+export default BadgeList;
+
+BadgeList.propTypes = {
+  sortedBadges: PropTypes.array.isRequired,
+  userBadges: PropTypes.array.isRequired,
+  handleBadgeModal: PropTypes.func.isRequired,
+};
--- src/pages/Member.jsx
+++ src/pages/Member.jsx
@@ -27,6 +27,7 @@ import { Navigate } from "react-router-dom";
 import { habitIcons, settingIcons } from "../assets/icons";
 import HabitList from "../components/HabitList";
 import AchievementList from "../components/AchievementList";
+import BadgeList from "../components/BadgeList";
 import CustomSelect from "../components/CustomSelect";
 import AchievementModal from "../components/AchievementModal";
 import BadgeModal from "../components/BadgeModal";
@@ -488,29 +489,7 @@ function Member() {
               <div className="w-full bg-light text-center rounded-2xl">{profileData.levelPoints}%</div>
             </div>
             <AchievementList sortedAchievements={sortedAchievements} userAchievements={userAchievements} handleAchievementModal={handleAchievementModal} />
-            <div className="pt-9 pb-4 px-4 bg-black-50 space-y-4 rounded-2xl relative">
-              <ul className="grid grid-cols-3 gap-4 md:grid-cols-6">
-                {sortedBadges.slice(0, 6).map((badge, index) => (
-                  <li key={badge.id} className="relative w-full h-fit">
-                    <img src={badge.image} alt={`Badge ${index}`} className={`w-full h-full object-cover ${userBadges.includes(badge.id) ? "opacity-100" : "opacity-30"}`} />
-                  </li>
-                ))}
-              </ul>
-              <button className="text-center w-full bg-primary rounded-xl font-medium text-sm leading-5 py-1 hover:bg-primary-dark" onClick={handleBadgeModal}>
-                更多獎勵徽章
-              </button>
-              <div className="bg-primary py-1 px-4 w-[150px] absolute -top-12 left-1/2 transform -translate-x-1/2">
-                <p className="font-lobster font-normal text-2xl leading-8 text-alert text-center">Badge</p>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-16px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-t-[13px] before:border-l-primary before:border-r-transparent before:border-t-primary-dark before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[114px] before:w-0 before:h-0 before:border-r-[20px] before:border-l-0 before:border-t-[13px] before:border-r-primary before:border-l-transparent before:border-t-primary-dark before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-46px] before:w-8 before:h-10 before:bg-primary"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[134px] before:w-8 before:h-10 before:bg-primary"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[166px] before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-b-[20px] before:border-l-transparent before:border-r-transparent before:border-b-primary before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-1 before:left-[166px] before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-t-[20px] before:border-l-transparent before:border-r-transparent before:border-t-primary before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-[-16px] before:left-[-66px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-b-[20px] before:border-l-transparent before:border-r-transparent before:border-b-primary before:z-20"></div>
-                <div className="absolute before:content-[''] before:absolute before:bottom-1 before:left-[-66px] before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-t-[20px] before:border-l-transparent before:border-r-transparent before:border-t-primary before:z-20"></div>
-              </div>
-            </div>
+            <BadgeList sortedBadges={sortedBadges} userBadges={userBadges} handleBadgeModal={handleBadgeModal} />
           </div>
         ) : (
           <HabitList habits={filteredHabits} habitCategories={habitCategories} handleDetailClick={handleDetailClick} />

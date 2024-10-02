364704480b227ef16b2c2801c3b1a70b46d380a1
chunjull <chunjuchein@gmail.com>
2024-10-02 16:19:50 +0800

    style: adjust archieved achievement's style in modal

--- src/components/AchievementModal.jsx
+++ src/components/AchievementModal.jsx
@@ -10,20 +10,24 @@ const AchievementModal = ({ handleAchievementModal, userAchievements, sortedAchi
       </div>
       <div className="max-h-56 overflow-scroll">
         <ul className="grid grid-cols-2 gap-4">
-          {sortedAchievements.map((achievement) => (
-            <li key={achievement.id}>
-              <div
+          {sortedAchievements.slice(0, 6).map((achievement) => {
+            const isAchieved = userAchievements.includes(achievement.id);
+            return (
+              <li
+                key={achievement.id}
                 className={`py-1 w-full flex justify-center items-center rounded-lg border-2 cursor-default ${
-                  userAchievements.includes(achievement.id)
-                    ? "opacity-100 border-primary-dark bg-light text-primary-dark bordered-achievement"
-                    : "opacity-50 border-black-500 bg-black-100 text-black-500"
+                  isAchieved ? "opacity-100 border-primary-dark bg-gradient-to-br from-primary via-primary-dark to-alert" : "opacity-50 border-black-500 bg-black-100"
                 }`}
               >
-                <div className="font-normal text-base leading-6">{achievement.name}</div>
-              </div>
-              <div className="mt-1 text-black-700 text-center font-normal text-xs leading-4 md:text-sm md:leading-5">{achievement.description}</div>
-            </li>
-          ))}
+                <p
+                  className={`font-normal text-base leading-6 md:text-xl md:leading-7 stroke-text ${isAchieved ? "text-primary z-0" : "text-black-500"}`}
+                  data-stroke={isAchieved ? achievement.name : ""}
+                >
+                  {achievement.name}
+                </p>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>

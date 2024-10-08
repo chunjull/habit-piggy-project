64ad70efeae1b3d66854b9def79009bdba7c3afc
chunjull <chunjuchein@gmail.com>
2024-10-08 10:18:27 +0800

    style: render user's level in ranks

--- src/pages/Rank.jsx
+++ src/pages/Rank.jsx
@@ -76,55 +76,63 @@ function Rank() {
     return userSavingsCounts;
   };
 
+  const calculateLevelAndPoints = (levelPoints) => {
+    const level = Math.floor(levelPoints / 100);
+    return { level };
+  };
+
   const renderTopTenUsers = (userCounts, type) => {
     const sortedUsers = [...userCounts].sort((a, b) => (type === "habit" ? b.completedCount - a.completedCount : b.totalSavings - a.totalSavings));
 
-    return sortedUsers.slice(0, 10).map((user, index) => (
-      <li key={user.uid} className="relative flex justify-between items-center py-2 px-4 bg-black-50 dark:bg-black-800 rounded-2xl hover:bg-black-0 dark:hover:bg-black-600">
-        <div className="flex items-center gap-3">
-          {index < 3 ? (
-            <div className="flex items-center gap-3">
-              <div className="absolute top-[-4px]">
-                <div className="w-10 h-[68px] flex justify-center items-center bg-primary relative">
-                  <p className="font-normal text-center text-3xl leading-9 font-lobster text-alert">{index + 1}</p>
-                  <div className="absolute before:content-[''] before:absolute before:bottom-[-34px] before:right-0 before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-b-[12px] before:border-l-transparent before:border-r-transparent before:border-b-black-50 dark:before:border-b-black-800 before:z-20"></div>
-                  <div className="absolute before:content-[''] before:absolute before:bottom-[-34px] before:left-0 before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-b-[12px] before:border-l-transparent before:border-r-transparent before:border-b-black-50 dark:before:border-b-black-800 before:z-20"></div>
-                  <div className="absolute before:content-[''] before:absolute before:bottom-[30px] before:left-5 before:w-0 before:h-0 before:border-l-0 before:border-r-[3px] before:border-b-[4px] before:border-l-transparent before:border-r-transparent before:border-b-primary-dark before:z-20"></div>
+    return sortedUsers.slice(0, 10).map((user, index) => {
+      const { level } = calculateLevelAndPoints(user.levelPoints);
+      return (
+        <li key={user.uid} className="relative flex justify-between items-center py-2 px-4 bg-black-50 dark:bg-black-800 rounded-2xl hover:bg-black-0 dark:hover:bg-black-600">
+          <div className="flex items-center gap-3">
+            {index < 3 ? (
+              <div className="flex items-center gap-3">
+                <div className="absolute top-[-4px]">
+                  <div className="w-10 h-[68px] flex justify-center items-center bg-primary relative">
+                    <p className="font-normal text-center text-3xl leading-9 font-lobster text-alert">{index + 1}</p>
+                    <div className="absolute before:content-[''] before:absolute before:bottom-[-34px] before:right-0 before:w-0 before:h-0 before:border-l-[20px] before:border-r-0 before:border-b-[12px] before:border-l-transparent before:border-r-transparent before:border-b-black-50 dark:before:border-b-black-800 before:z-20"></div>
+                    <div className="absolute before:content-[''] before:absolute before:bottom-[-34px] before:left-0 before:w-0 before:h-0 before:border-l-0 before:border-r-[20px] before:border-b-[12px] before:border-l-transparent before:border-r-transparent before:border-b-black-50 dark:before:border-b-black-800 before:z-20"></div>
+                    <div className="absolute before:content-[''] before:absolute before:bottom-[30px] before:left-5 before:w-0 before:h-0 before:border-l-0 before:border-r-[3px] before:border-b-[4px] before:border-l-transparent before:border-r-transparent before:border-b-primary-dark before:z-20"></div>
+                  </div>
+                </div>
+                <img src={user.avatar} alt="user's avatar" className="w-12 h-12 rounded-full ml-14 outline outline-primary-dark dark:outline-primary" />
+                <div>
+                  <p className="font-normal text-lg leading-7 text-black dark:text-black-0">{user.name}</p>
+                  <p className="font-normal text-sm leading-5 text-black-700 dark:text-black-200">Lv. {level}</p>
                 </div>
               </div>
-              <img src={user.avatar} alt="user's avatar" className="w-12 h-12 rounded-full ml-14 outline outline-primary-dark dark:outline-primary" />
-              <div>
-                <p className="font-normal text-base leading-6 text-black dark:text-black-0">{user.name}</p>
-                {/* <p>Lv. {user.levelPoints}</p> */}
+            ) : (
+              <div className="flex items-center gap-3">
+                <p className="w-10 h-auto font-normal text-center text-3xl leading-9 font-lobster text-black dark:text-black-0">{index + 1}</p>
+                <img src={user.avatar} alt="user's avatar" className="w-12 h-12 rounded-full outline outline-primary-dark dark:outline-primary" />
+                <div>
+                  <p className="font-normal text-lg leading-7 text-black dark:text-black-0">{user.name}</p>
+                  <p className="font-normal text-sm leading-5 text-black-700 dark:text-black-200">Lv. {level}</p>
+                </div>
               </div>
-            </div>
-          ) : (
-            <div className="flex items-center gap-3">
-              <p className="w-10 h-auto font-normal text-center text-3xl leading-9 font-lobster text-black dark:text-black-0">{index + 1}</p>
-              <img src={user.avatar} alt="user's avatar" className="w-12 h-12 rounded-full outline outline-primary-dark dark:outline-primary" />
-              <div>
-                <p className="font-normal text-base leading-6 text-black dark:text-black-0">{user.name}</p>
-                {/* <p>Lv. {user.levelPoints}</p> */}
+            )}
+          </div>
+          <div className="flex flex-col items-end">
+            <p className="font-normal text-sm leading-5 text-black dark:text-black-0">{type === "habit" ? "累積數量" : "累積存款"}</p>
+            {type === "habit" ? (
+              <div className="flex items-center gap-1">
+                <p className="font-bold text-base leading-6 text-black dark:text-black-0">{user.completedCount}</p>
+                <p className="font-normal text-sm leading-5 text-black dark:text-black-0">次</p>
               </div>
-            </div>
-          )}
-        </div>
-        <div className="flex flex-col items-end">
-          <p className="font-normal text-sm leading-5 text-black dark:text-black-0">{type === "habit" ? "累積數量" : "累積存款"}</p>
-          {type === "habit" ? (
-            <div className="flex items-center gap-1">
-              <p className="font-bold text-base leading-6 text-black dark:text-black-0">{user.completedCount}</p>
-              <p className="font-normal text-sm leading-5 text-black dark:text-black-0">次</p>
-            </div>
-          ) : (
-            <div className="flex items-center gap-1">
-              <p className="font-bold text-base leading-6 text-black dark:text-black-0">{user.totalSavings}</p>
-              <p className="font-normal text-sm leading-5 text-black dark:text-black-0">元</p>
-            </div>
-          )}
-        </div>
-      </li>
-    ));
+            ) : (
+              <div className="flex items-center gap-1">
+                <p className="font-bold text-base leading-6 text-black dark:text-black-0">{user.totalSavings}</p>
+                <p className="font-normal text-sm leading-5 text-black dark:text-black-0">元</p>
+              </div>
+            )}
+          </div>
+        </li>
+      );
+    });
   };
 
   const renderCurrentUser = (userCounts, currentUser, type) => {
@@ -133,14 +141,15 @@ function Rank() {
       return null;
     }
     const rank = userCounts.findIndex((user) => user.uid === currentUser.uid) + 1;
+    const { level } = calculateLevelAndPoints(currentUserData.levelPoints);
     return (
       <div className="flex justify-between py-2 px-4 bg-black-500 dark:bg-black-50 rounded-2xl">
         <div className="flex items-center gap-3 text-black-0 dark:text-black">
           <p className="w-10 h-auto font-normal text-center text-3xl leading-9 font-lobster text-black-0 dark:text-black">{rank}</p>
           <img src={currentUserData.avatar} alt="user's avatar" className="w-12 h-12 rounded-full outline dark:outline-primary-dark outline-primary" />
           <div>
-            <p className="font-normal text-base leading-6 text-white dark:text-black">{currentUserData.name}</p>
-            {/* <p>Lv. {currentUserData.levelPoints}</p> */}
+            <p className="font-normal text-lg leading-7 text-white dark:text-black">{currentUserData.name}</p>
+            <p className="font-normal text-sm leading-5 text-black-700">Lv. {level}</p>
           </div>
         </div>
         <div className="flex flex-col items-end text-black-0 dark:text-black">

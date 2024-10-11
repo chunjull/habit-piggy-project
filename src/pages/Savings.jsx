97c88dc51f08787947b9e51fcc048d9a3100c2a7
chunjull <chunjuchein@gmail.com>
2024-10-11 17:14:46 +0800

    style: adjust savings page layout

--- src/pages/Savings.jsx
+++ src/pages/Savings.jsx
@@ -221,16 +221,16 @@ function Savings() {
     return uncompletedHabits.slice(0, 50).map((habit, index) => (
       <li key={habit.id + habit.date} className="py-2 px-4 grid grid-cols-5 bg-black-50 dark:bg-black-800 rounded-lg font-normal text-sm leading-5 hover:bg-black-0 dark:hover:bg-black-600">
         <p className="text-black dark:text-black-0">{String(index + 1).padStart(2, "0")}</p>
-        <p className="text-center text-black dark:text-black-0">{new Date(habit.date).toLocaleDateString()}</p>
-        <p className="text-center col-span-2 overflow-scroll text-black dark:text-black-0">{habit.title}</p>
-        <p className="text-center text-black dark:text-black-0">NT${habit.amount}</p>
+        <p className="text-center text-black dark:text-black-0 overflow-scroll">{new Date(habit.date).toLocaleDateString()}</p>
+        <p className="text-center col-span-2 text-black dark:text-black-0 truncate">{habit.title}</p>
+        <p className="text-center text-black dark:text-black-0 overflow-scroll">NT${habit.amount}</p>
       </li>
     ));
   };
 
   return (
     <div className="p-4 md:py-10 space-y-4">
-      <ul className="grid grid-cols-3 w-full">
+      <ul className="grid grid-cols-2 w-full">
         <li
           className={`border border-black-500 rounded-s-full py-1 font-normal text-sm leading-5 text-center ${isActiveTab === "overview" ? "bg-primary" : "bg-black-50"}`}
           onClick={() => setIsActiveTab("overview")}
@@ -238,16 +238,10 @@ function Savings() {
           存款總覽
         </li>
         <li
-          className={`border-e border-y border-black-500 py-1 font-normal text-sm leading-5 text-center ${isActiveTab === "category" ? "bg-primary" : "bg-black-50"}`}
+          className={`border-e border-y border-black-500 rounded-e-full py-1 font-normal text-sm leading-5 text-center ${isActiveTab === "category" ? "bg-primary" : "bg-black-50"}`}
           onClick={() => setIsActiveTab("category")}
         >
-          習慣類別總覽
-        </li>
-        <li
-          className={`border-e border-y border-black-500 rounded-e-full py-1 font-normal text-sm leading-5 text-center ${isActiveTab === "type" ? "bg-primary" : "bg-black-50"}`}
-          onClick={() => setIsActiveTab("type")}
-        >
-          習慣類型總覽
+          習慣養成總覽
         </li>
       </ul>
       {isActiveTab === "overview" && (
@@ -289,83 +283,83 @@ function Savings() {
         </div>
       )}
       {isActiveTab === "category" && (
-        <div className={`p-4 bg-black-50 dark:bg-black-800 rounded-xl space-y-4 ${savingsCount === 0 ? "min-h-screen" : "h-fit"}`}>
-          <div className="flex justify-between items-center">
-            <div className="relative group flex items-center flex-grow">
-              <h2 className="font-bold text-xl leading-7 text-black dark:text-black-0">習慣類別總覽</h2>
-              <modalIcons.TbInfoCircle className="w-6 h-6 text-black-500 dark:text-black-200 ml-2 inline-block" />
-              <span className="absolute top-0 left-[168px] transform -translate-x-0 w-fit p-2 bg-primary-dark text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none group-hover:pointer-events-auto z-50 before:content-[''] before:absolute before:top-7 before:-left-4 before:transform before:-translate-y-full before:border-8 before:border-transparent before:border-r-primary-dark whitespace-normal break-words">
-                來看看你的習慣類別分布吧！哪些類別的習慣讓你累積最多存款呢？
-              </span>
-            </div>
-            <div className="relative">
-              <CustomSelect options={options} value={filter} onChange={setFilter} />
-            </div>
-          </div>
-          {savingsCount === 0 ? (
-            <div className="space-y-2">
-              <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">找不到相關的存款資料</p>
-              <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">要不要試著培養一些習慣呢？</p>
-            </div>
-          ) : (
-            <div>
-              <div className="w-full h-[400px]">
-                <CategoryChart categoryData={categoryData} habitCategories={habitCategories} />
+        <div className="space-y-4">
+          <div className={`p-4 bg-black-50 dark:bg-black-800 rounded-xl space-y-4 ${savingsCount === 0 ? "min-h-screen" : "h-fit"}`}>
+            <div className="flex justify-between items-center">
+              <div className="relative group flex items-center flex-grow">
+                <h2 className="font-bold text-xl leading-7 text-black dark:text-black-0">類別總覽</h2>
+                <modalIcons.TbInfoCircle className="w-6 h-6 text-black-500 dark:text-black-200 ml-2 inline-block" />
+                <span className="absolute top-0 left-[168px] transform -translate-x-0 w-fit p-2 bg-primary-dark text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none group-hover:pointer-events-auto z-50 before:content-[''] before:absolute before:top-7 before:-left-4 before:transform before:-translate-y-full before:border-8 before:border-transparent before:border-r-primary-dark whitespace-normal break-words">
+                  來看看你的習慣類別分布吧！哪些類別的習慣讓你累積最多存款呢？
+                </span>
               </div>
-              <ul className="space-y-2">
-                {habitCategories.map((category) => (
-                  <li key={category.id} className="p-2 rounded flex justify-between items-center hover:bg-black-0 dark:hover:bg-black-600">
-                    <div className="flex items-center gap-3">
-                      <div className={`w-6 h-6 rounded-full`} style={{ backgroundColor: category.color }}></div>
-                      <p className="font-normal text-base leading-6 text-black dark:text-black-0">{category.name}</p>
-                    </div>
-                    <p className="font-bold text-lg leading-6 text-black dark:text-black-0">
-                      {(((categoryData[category.id] || 0) / Object.values(categoryData).reduce((a, b) => a + b, 0)) * 100).toFixed(1)}%
-                    </p>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      )}
-      {isActiveTab === "type" && (
-        <div className={`p-4 bg-black-50 dark:bg-black-800 rounded-xl space-y-4 ${savingsCount === 0 ? "min-h-screen" : "h-fit"}`}>
-          <div className="flex justify-between items-center">
-            <div className="relative group flex items-center flex-grow">
-              <h2 className="font-bold text-xl leading-7 text-black dark:text-black-0">習慣類型總覽</h2>
-              <modalIcons.TbInfoCircle className="w-6 h-6 text-black-500 dark:text-black-200 ml-2 inline-block" />
-              <span className="absolute top-0 left-[168px] transform -translate-x-0 w-fit p-2 bg-primary-dark text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none group-hover:pointer-events-auto z-50 before:content-[''] before:absolute before:top-7 before:-left-4 before:transform before:-translate-y-full before:border-8 before:border-transparent before:border-r-primary-dark whitespace-normal break-words">
-                來看看你的習慣類型分布吧！你比較擅長養成還是戒除習慣呢？
-              </span>
-            </div>
-            <div className="relative">
-              <CustomSelect options={options} value={filter} onChange={setFilter} />
-            </div>
-          </div>
-          {savingsCount === 0 ? (
-            <div className="space-y-2">
-              <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">找不到相關的存款資料</p>
-              <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">要不要試著培養一些習慣呢？</p>
-            </div>
-          ) : (
-            <div>
-              <div className="w-full h-[400px]">
-                <TypeChart typeData={typeData} habitType={habitType} />
+              <div className="relative">
+                <CustomSelect options={options} value={filter} onChange={setFilter} />
               </div>
-              <ul className="space-y-2">
-                {habitType.map((type) => (
-                  <li key={type.id} className="p-2 rounded flex justify-between items-center hover:bg-black-0 dark:hover:bg-black-600">
-                    <div className="flex items-center gap-3">
-                      <div className={`w-6 h-6 rounded-full`} style={{ backgroundColor: type.color }}></div>
-                      <p className="font-normal text-base leading-6 text-black dark:text-black-0">{type.name}</p>
-                    </div>
-                    <p className="font-bold text-lg leading-6 text-black dark:text-black-0">{(((typeData[type.id] || 0) / Object.values(typeData).reduce((a, b) => a + b, 0)) * 100).toFixed(1)}%</p>
-                  </li>
-                ))}
-              </ul>
             </div>
-          )}
+            {savingsCount === 0 ? (
+              <div className="space-y-2">
+                <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">找不到相關的存款資料</p>
+                <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">要不要試著培養一些習慣呢？</p>
+              </div>
+            ) : (
+              <div>
+                <div className="w-full h-[400px]">
+                  <CategoryChart categoryData={categoryData} habitCategories={habitCategories} />
+                </div>
+                <ul className="space-y-2">
+                  {habitCategories.map((category) => (
+                    <li key={category.id} className="p-2 rounded flex justify-between items-center hover:bg-black-0 dark:hover:bg-black-600">
+                      <div className="flex items-center gap-3">
+                        <div className={`w-6 h-6 rounded-full`} style={{ backgroundColor: category.color }}></div>
+                        <p className="font-normal text-base leading-6 text-black dark:text-black-0">{category.name}</p>
+                      </div>
+                      <p className="font-bold text-lg leading-6 text-black dark:text-black-0">
+                        {(((categoryData[category.id] || 0) / Object.values(categoryData).reduce((a, b) => a + b, 0)) * 100).toFixed(1)}%
+                      </p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+          <div className={`p-4 bg-black-50 dark:bg-black-800 rounded-xl space-y-4 ${savingsCount === 0 ? "min-h-screen" : "h-fit"}`}>
+            <div className="flex justify-between items-center">
+              <div className="relative group flex items-center flex-grow">
+                <h2 className="font-bold text-xl leading-7 text-black dark:text-black-0">類型總覽</h2>
+                <modalIcons.TbInfoCircle className="w-6 h-6 text-black-500 dark:text-black-200 ml-2 inline-block" />
+                <span className="absolute top-0 left-[168px] transform -translate-x-0 w-fit p-2 bg-primary-dark text-white text-sm rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none group-hover:pointer-events-auto z-50 before:content-[''] before:absolute before:top-7 before:-left-4 before:transform before:-translate-y-full before:border-8 before:border-transparent before:border-r-primary-dark whitespace-normal break-words">
+                  來看看你的習慣類型分布吧！你比較擅長養成還是戒除習慣呢？
+                </span>
+              </div>
+              <div className="relative">
+                <CustomSelect options={options} value={filter} onChange={setFilter} />
+              </div>
+            </div>
+            {savingsCount === 0 ? (
+              <div className="space-y-2">
+                <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">找不到相關的存款資料</p>
+                <p className="text-center font-normal text-base leading-6 text-black dark:text-black-0">要不要試著培養一些習慣呢？</p>
+              </div>
+            ) : (
+              <div>
+                <div className="w-full h-[400px]">
+                  <TypeChart typeData={typeData} habitType={habitType} />
+                </div>
+                <ul className="space-y-2">
+                  {habitType.map((type) => (
+                    <li key={type.id} className="p-2 rounded flex justify-between items-center hover:bg-black-0 dark:hover:bg-black-600">
+                      <div className="flex items-center gap-3">
+                        <div className={`w-6 h-6 rounded-full`} style={{ backgroundColor: type.color }}></div>
+                        <p className="font-normal text-base leading-6 text-black dark:text-black-0">{type.name}</p>
+                      </div>
+                      <p className="font-bold text-lg leading-6 text-black dark:text-black-0">{(((typeData[type.id] || 0) / Object.values(typeData).reduce((a, b) => a + b, 0)) * 100).toFixed(1)}%</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       )}
     </div>

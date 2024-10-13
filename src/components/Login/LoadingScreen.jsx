fb0b23794e26682cbe3682c4d39565f8d4fd7502
chunjull <chunjuchein@gmail.com>
2024-10-14 07:10:06 +0800

    refactor: split loading animation component

---
+++ src/components/Login/LoadingScreen.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import habitPiggyLoading1 from "../../assets/images/habit-piggy-loading-1.svg";
+import habitPiggyLoading2 from "../../assets/images/habit-piggy-loading-2.svg";
+import PropTypes from "prop-types";
+
+const LoadingScreen = ({ isLoading }) => {
+  const [currentImage, setCurrentImage] = useState(habitPiggyLoading1);
+
+  useEffect(() => {
+    let interval;
+    if (isLoading) {
+      interval = setInterval(() => {
+        setCurrentImage((prevImage) => (prevImage === habitPiggyLoading1 ? habitPiggyLoading2 : habitPiggyLoading1));
+      }, 250);
+    }
+    return () => clearInterval(interval);
+  }, [isLoading]);
+
+  if (!isLoading) return null;
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-4">
+      <img src={currentImage} alt="loading" className="w-40 h-40 object-cover" />
+      <p className="font-normal text-base text-black dark:text-black-0">準備好培養習慣了嗎？</p>
+    </div>
+  );
+};
+
+export default LoadingScreen;
+
+LoadingScreen.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+};
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,31 +1,19 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { registerUser, getEmailByAccount } from "../services/api";
 import { AuthContext } from "../utils/AuthContext";
 import { Navigate } from "react-router-dom";
-import habitPiggyLoading1 from "../assets/images/habit-piggy-loading-1.svg";
-import habitPiggyLoading2 from "../assets/images/habit-piggy-loading-2.svg";
 import TabNavigation from "../components/Login/TabNavigation";
 import LoginForm from "../components/Login/LoginForm";
 import RegisterForm from "../components/Login/RegisterForm";
+import LoadingScreen from "../components/Login/LoadingScreen";
 
 function Login() {
   const [isLogin, setIsLogin] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [currentImage, setCurrentImage] = useState(habitPiggyLoading1);
   const { setUser } = useContext(AuthContext);
   const [loginError, setLoginError] = useState("");
-
-  useEffect(() => {
-    let interval;
-    if (isLoading) {
-      interval = setInterval(() => {
-        setCurrentImage((prevImage) => (prevImage === habitPiggyLoading1 ? habitPiggyLoading2 : habitPiggyLoading1));
-      }, 250);
-    }
-    return () => clearInterval(interval);
-  }, [isLoading]);
 
   const handleRegister = async (data) => {
     const { email, registerPassword, account, name } = data;
@@ -69,20 +57,17 @@ function Login() {
     return <Navigate to="/home" />;
   }
 
-  if (isLoading) {
-    return (
-      <div className="flex flex-col justify-center items-center h-screen gap-4">
-        <img src={currentImage} alt="loading" className="w-40 h-40 object-cover" />
-        <p className="font-normal text-base text-black dark:text-black-0">準備好培養習慣了嗎？</p>
-      </div>
-    );
-  }
-
   return (
-    <div className="p-4 md:py-10 space-y-4">
-      <TabNavigation isLogin={isLogin} setIsLogin={setIsLogin} />
-      {isLogin ? <LoginForm handleLogin={handleLogin} loginError={loginError} /> : <RegisterForm handleRegister={handleRegister} />}
-    </div>
+    <>
+      {isLoading ? (
+        <LoadingScreen isLoading={isLoading} />
+      ) : (
+        <div className="p-4 md:py-10 space-y-4">
+          <TabNavigation isLogin={isLogin} setIsLogin={setIsLogin} />
+          {isLogin ? <LoginForm handleLogin={handleLogin} loginError={loginError} /> : <RegisterForm handleRegister={handleRegister} />}
+        </div>
+      )}
+    </>
   );
 }
 

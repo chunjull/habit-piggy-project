ada08ee0983763cf4446a80ee161f8cdb3a4e44b
chunjull <chunjuchein@gmail.com>
2024-10-15 19:05:22 +0800

    style: adjust error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+import HabitPiggyLogo from "../assets/images/habit-piggy-logo.svg";
+
 function Error() {
   return (
-    <div>
-      <h1>Error</h1>
+    <div className="flex flex-col justify-center items-center h-full">
+      <p className="font-normal text-lg leading-6 py-2">{`There's Nothing Delicious.`}</p>
+      <img src={HabitPiggyLogo} alt="Habit Piggy Logo" className="w-60 h-60" />
+      <Link to="/" className="font-normal text-lg leading-6 py-2 px-4 bg-primary rounded-2xl hover:bg-primary-dark">
+        Do Habit
+      </Link>
     </div>
   );
 }

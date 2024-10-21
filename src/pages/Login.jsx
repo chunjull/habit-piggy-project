9003cb7d82f97bb4c76a6ca4737d5650867865cc
chunjull <chunjuchein@gmail.com>
2024-10-21 10:23:10 +0800

    chore: install prettier-plugin-tailwindcss

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,7 +21,11 @@ function Login() {
     try {
       await registerUser(email, registerPassword, account, name);
       const auth = getAuth();
-      const userCredential = await signInWithEmailAndPassword(auth, email, registerPassword);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        registerPassword,
+      );
       setUser(userCredential.user);
       dispatch({ type: actionTypes.SET_IS_LOADING, payload: true });
       setTimeout(() => {
@@ -38,18 +42,28 @@ function Login() {
     try {
       const email = await getEmailByAccount(loginAccount);
       if (!email) {
-        dispatch({ type: actionTypes.SET_LOGIN_ERROR, payload: "帳號或密碼錯誤" });
+        dispatch({
+          type: actionTypes.SET_LOGIN_ERROR,
+          payload: "帳號或密碼錯誤",
+        });
         return;
       }
 
-      const userCredential = await signInWithEmailAndPassword(auth, email, loginPassword);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        loginPassword,
+      );
       setUser(userCredential.user);
       dispatch({ type: actionTypes.SET_IS_LOADING, payload: true });
       setTimeout(() => {
         dispatch({ type: actionTypes.SET_IS_LOGGED_IN, payload: true });
       }, 2000);
     } catch (error) {
-      dispatch({ type: actionTypes.SET_LOGIN_ERROR, payload: "帳號或密碼錯誤" });
+      dispatch({
+        type: actionTypes.SET_LOGIN_ERROR,
+        payload: "帳號或密碼錯誤",
+      });
       console.error("Error logging in: ", error.code, error.message);
     }
   };
@@ -63,11 +77,23 @@ function Login() {
       {state.isLoading ? (
         <LoadingScreen isLoading={state.isLoading} />
       ) : (
-        <div className="flex flex-col justify-between h-full">
+        <div className="flex h-full flex-col justify-between">
           <Header />
-          <div className="p-4 md:py-8 space-y-4 w-full md:w-1/2 h-fit md:bg-black-50 md:mx-auto rounded-2xl">
-            <TabNavigation isLogin={state.isLogin} setIsLogin={(value) => dispatch({ type: actionTypes.SET_IS_LOGIN, payload: value })} />
-            {state.isLogin ? <LoginForm handleLogin={handleLogin} loginError={state.loginError} /> : <RegisterForm handleRegister={handleRegister} />}
+          <div className="h-fit w-full space-y-4 rounded-2xl p-4 md:mx-auto md:w-1/2 md:bg-black-50 md:py-8">
+            <TabNavigation
+              isLogin={state.isLogin}
+              setIsLogin={(value) =>
+                dispatch({ type: actionTypes.SET_IS_LOGIN, payload: value })
+              }
+            />
+            {state.isLogin ? (
+              <LoginForm
+                handleLogin={handleLogin}
+                loginError={state.loginError}
+              />
+            ) : (
+              <RegisterForm handleRegister={handleRegister} />
+            )}
           </div>
           {state.isLogin && <LoginBanner />}
           <Footer />
